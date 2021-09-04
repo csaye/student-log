14bb406e8f5d40a84a5d9429535a6afa9ae9710c
Cooper Saye <coopersaye@gmail.com>
2021-09-03 21:52:58 -0700

    Update script.

--- scripts/start-entry.sh
+++ scripts/start-entry.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 FILENAME="../logs/$(date +'%Y-%m-%d').log.md"
 touch $FILENAME
-echo "# $(date +'%A, %B %e, %Y %I:%M %p')" >> $FILENAME
+echo "# $(date +'%A, %B%e, %Y %I:%M %p')" >> $FILENAME
 echo "- [ ]" >> $FILENAME
-code $FILENAME
