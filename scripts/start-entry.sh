fbc7d823be5dc5e5acebaf5f71177a4ecd2c2c1b
Cooper Saye <coopersaye@gmail.com>
2021-09-18 14:33:53 -0700

    Fix script date representation.

--- scripts/start-entry.sh
+++ scripts/start-entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 FILENAME="../logs/$(date +'%Y-%m-%d').log.md"
 touch $FILENAME
-echo "# $(date +'%A, %B%e, %Y %I:%M %p')" >> $FILENAME
+echo "# $(date +'%A, %B %d, %Y %I:%M %p')" >> $FILENAME
 echo "- [ ]" >> $FILENAME
